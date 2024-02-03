9f900b751868b1999d7f36f7f90e99a9d7241dd6
carter-h-ross <119367706+carter-h-ross@users.noreply.github.com>
2024-02-02 21:00:33 -0800

    Add files via upload

--- src/game.js
+++ src/game.js
@@ -24,10 +24,6 @@ import {
     onAuthStateChanged
 } from 'firebase/auth'
 
-const dr = getDatabase();
-const auth = getAuth();
-var userId = null;
-
 const firebaseConfig = {
     apiKey: "AIzaSyChxw12x7mVWQxXkFou37p8thMQ8Tjg_q0",
     authDomain: "carterross-dev-pacman.firebaseapp.com",
@@ -46,9 +42,14 @@ function getFullLeaderboard(mapId, userId, date) {
 
 }
 
-function load100(start, end) {
+function loadScores(start, end) {
 
 }
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+const auth = getAuth();
+var userId = null;
 
 /* ------------------------------------------------------------------------- A* ----------------------------------------------------------------------*/
 
@@ -485,42 +486,136 @@ class Ghost {
     
 }
 
-class World {
-
-    constructor(dimensions, map, skybox, id) {
-        self.dimensions = dimensions;
-        self.map = map;
-        self.skybox = skybox;
-        self.ref = id;
-    }
-
-    load() {
-
-    }
-    
-}
-
-
 class Map {
-    constructor(codes) {
+    constructor(codes, floorTexture, wallTexture, lightsIntensity) {
         this.codes = codes;
         this.sizeX = codes[0][0].length;
         this.sizeZ = codes[0].length;
         this.sizeY = codes.length;
+        this.floorTexture = `${floorTexture}-floor`;
+        this.wallTexture = `${wallTexture}-wall`;
+        this.lightsIntensity = lightsIntensity;
+    }
+
+    load() {
+
+        
+        console.log(this.codes);
+        /*
+        const ambientLight = new THREE.AmbientLight(0xFFFFFF);
+        scene.add(ambientLight);
+        */
+
+        // Directional Lights
+        const directionalLight1 = new THREE.DirectionalLight(0xffffff, this.lightsIntensity);
+        directionalLight1.position.set(this.sizeX * blockScale, this.sizeY * blockScale * 2, this.sizeZ * blockScale);
+        directionalLight1.target.position.set((this.sizeX * blockScale) / 2, 0, (this.sizeZ * blockScale) / 2);
+        directionalLight1.castShadow = true;
+        scene.add(directionalLight1);
+
+        const directionalLight2 = new THREE.DirectionalLight(0xffffff, this.lightsIntensity);
+        directionalLight2.position.set(this.sizeX * blockScale, this.sizeY * blockScale * 2, -2);
+        directionalLight2.target.position.set((this.sizeX * blockScale) / 2, 0, (this.sizeZ * blockScale) / 2);
+        directionalLight2.castShadow = true;
+        scene.add(directionalLight2);
+
+        const directionalLight3 = new THREE.DirectionalLight(0xffffff, this.lightsIntensity);
+        directionalLight3.position.set(-2, this.sizeY * blockScale * 2, this.sizeZ * blockScale);
+        directionalLight3.target.position.set((this.sizeX * blockScale) / 2, 0, (this.sizeZ * blockScale) / 2);
+        directionalLight3.castShadow = true;
+        scene.add(directionalLight3);
+
+        const directionalLight4 = new THREE.DirectionalLight(0xffffff, this.lightsIntensity);
+        directionalLight4.position.set(-2, this.sizeY * blockScale * 2, -2);
+        directionalLight4.target.position.set((this.sizeX * blockScale) / 2, 0, (this.sizeZ * blockScale) / 2);
+        directionalLight4.castShadow = true;
+        scene.add(directionalLight4);
+
+        // Enable shadows for the entire scene
+        renderer.shadowMap.enabled = true;
+
+        const light = new THREE.PointLight( 0xff0000, 1, 100 );
+        light.position.set( 50, 50, 50 );
+        scene.add( light );
+
+        // loading floor blocks
+        let floorCount = 0;
+        for (let z = 0;z < this.sizeZ;z++) {
+            for (let x = 0;x < this.sizeX;x++) {
+                if (this.codes[0][z][x] == 1) {
+                    floorCount++;       
+                }
+            }
+        }
+        const floorMesh = new THREE.InstancedMesh(blockGeometry, textures[this.floorTexture], floorCount);
+        scene.add(floorMesh);
+        floorCount = 0;
+        const tempFloor = new THREE.Object3D();
+        for (let z = 0;z < this.sizeZ;z++) {
+            for (let x = 0;x < this.sizeX;x++) {
+                if (this.codes[0][z][x] == 1) {
+                    tempFloor.position.x = x * blockScale;
+                    tempFloor.position.z = z * blockScale;            
+                    tempFloor.position.y = 0;
+                    tempFloor.updateMatrix();
+                    floorMesh.setMatrixAt(floorCount, tempFloor.matrix);
+                    floorCount++;
+                }
+            }
+        }
+
+        // loading wall blocks
+        let wallCount = 0;
+        for (let y = 1;y < this.sizeY;y++) {
+            for (let z = 0;z < this.sizeZ;z++) {
+                for (let x = 0;x < this.sizeX;x++) {
+                    if (this.codes[y][z][x] == 2) {
+                        wallCount++;       
+                    }
+                }
+            }
+        }
+        const wallMesh = new THREE.InstancedMesh(blockGeometry, textures[this.wallTexture], wallCount);
+        scene.add(wallMesh);
+        wallCount = 0;
+        const tempWall = new THREE.Object3D();
+        for (let y = 1;y < this.sizeY;y++) {
+            for (let z = 0;z < this.sizeZ;z++) {
+                for (let x = 0;x < this.sizeX;x++) {
+                    if (this.codes[y][z][x] == 2) {
+                        tempWall.position.x = x * blockScale;
+                        tempWall.position.z = z * blockScale;            
+                        tempWall.position.y = y * blockScale;
+                        tempWall.updateMatrix();
+                        wallMesh.setMatrixAt(wallCount, tempWall.matrix);
+                        wallCount++;
+                    }
+                }
+            }
+        }
+    }
+
+    pushEmptyArray() {
+        const emptyArray = Array.from({ length: this.sizeX }, () => 0);
+        const newArray = Array.from({ length: this.sizeZ }, () => [...emptyArray]);
+        this.codes.push(newArray);
+        return this;
     }
 
     buildWalls(height) {
-        for (let y = 0; y < height; y++) {
+        for (let y = 1; y < height+1; y++) {
+            this.pushEmptyArray();
             let newMap = JSON.parse(JSON.stringify(this.codes)); // Create a deep copy of the current map
-            for (let x = 1; x < this.sizeX - 1; x++) {
-                for (let z = 1; z < this.sizeZ - 1; z++) {
-                    if (this.codes[y][z][x] === 0 && this.isAdjacentToBlock(x, z, y)) {
-                        newMap[y][z][x] = 1;
+            for (let x = 0; x < this.sizeX; x++) {
+                for (let z = 0; z < this.sizeZ; z++) {
+                    if (this.codes[0][z][x] == 0) {
+                        newMap[y][z][x] = 2;
                     }
                 }
             }
             this.codes = newMap;
         }
+        this.sizeY = height + 1;
         return this;
     }
 
@@ -557,6 +652,7 @@ class Map {
         if (this.codes[0][z4][x4] == 0) {
             alert("invalid ghost placement on map");
         }
+        return this;
     }
 
     addPlayer(x,z,dir) {
@@ -573,27 +669,61 @@ class Map {
         if (this.codes[0][z][x] == 0) {
             alert("invalid player placement on map");
         }
+        return this;
     }
 }
 
 const scene = new THREE.Scene();
 const gltfLoader = new GLTFLoader();
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector("#bg"),
+});
 
-testMap = new Map([
+function printDebugWorldMap (map) {
+    console.log(map);
+    for (let y = 0;y < map.length;y++) {
+        for (let z = 0;z < map[0].length;z++) {
+            let row = "";
+            for (let x = 0;x < map[0][0].length;x++) {
+                switch (map[y][z][x] == 0) {
+                    case '0':
+                        row += "-";
+                    case '1':
+                        row += "X"
+                    default:
+                        row += "{"
+                }
+            }
+            console.log(row);
+        }
+    }
+    console.log("\n");
+}
+
+const textureLoader = new THREE.TextureLoader();
+const textures = {
+    "red-black-wall": new THREE.MeshStandardMaterial({map: textureLoader.load("textures/red-black/wall.jpg")}),
+    "red-black-floor": new THREE.MeshStandardMaterial({map: textureLoader.load("textures/red-black/floor.jpg")}),
+}
+
+const blockScale = 1; 
+const blockGeometry = new THREE.BoxGeometry( blockScale, blockScale, blockScale);
+
+let testMap = new Map([
 [//  0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5  6  7  8  9
     [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 1
     [ 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0], // 2
     [ 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0], // 3
-    [ 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0], // 4
-    [ 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0], // 5
-    [ 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0], // 6
-    [ 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0], // 7
-    [ 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0], // 8
-    [ 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0], // 9
-    [ 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0], // 0
-    [ 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0], // 1
-    [ 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0], // 2
-    [ 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0], // 3
+    [ 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0], // 4
+    [ 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0], // 5
+    [ 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0], // 6
+    [ 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0], // 7
+    [ 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0], // 8
+    [ 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0], // 9
+    [ 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0], // 0
+    [ 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0], // 1
+    [ 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0], // 2
+    [ 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0], // 3
     [ 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0], // 4
     [ 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0], // 5
     [ 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0], // 6
@@ -602,4 +732,20 @@ testMap = new Map([
     [ 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0], // 9
     [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 0
 ]
-]).buildWalls(2).addGhosts(1,1, 1,18, 18,1, 18,18).addPlayer(15, 9, "w");
+], "red-black", "red-black", 0.2).buildWalls(2).addGhosts(1,1, 1,18, 18,1, 18,18).addPlayer(10, 5, "w").load();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 20;
+camera.position.y = 30;
+camera.position.x = 20;
+camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+orbit.update();
+  
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+function animate() {
+    renderer.render(scene, camera);
+}
+renderer.setAnimationLoop(animate);
